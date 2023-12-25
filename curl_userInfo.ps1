6ef5db6ead9cb07666cfce2d2470d938f0597ff1
Rim <anonymous@mail.com>
2023-12-25 05:19:22 -0500

    Add regex to sanitize userInfo

--- curl_userInfo.ps1
+++ curl_userInfo.ps1
@@ -4,4 +4,4 @@ $URL = "https://profile.callofduty.com/cod/userInfo/$COOKIE_VALUE"
 $USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 $OUTPUT_FILE = "userInfo.json"
 
-curl -v $URL -H "Cookie: ACT_SSO_COOKIE=$COOKIE_VALUE"  -H "User-Agent: $USER_AGENT" -o $OUTPUT_FILE
+curl -v $URL -H "Cookie: ACT_SSO_COOKIE=$COOKIE_VALUE" -H "User-Agent: $USER_AGENT" -o $OUTPUT_FILE
