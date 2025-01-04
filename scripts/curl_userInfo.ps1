c3e0db32d8b79cb4fabf416a46ad03613a33bf80
Rim <mrcodblackops10>
2025-01-04 11:27:34 -0500

    maint(curl_userInfo.ps1): update curl command - must include cookies.txt file now

--- scripts/curl_userInfo.ps1
+++ scripts/curl_userInfo.ps1
@@ -1,7 +1,6 @@
 $COOKIE_VALUE = ""
-
 $URL = "https://profile.callofduty.com/cod/userInfo/$COOKIE_VALUE"
-$USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+$USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 $OUTPUT_FILE = "userInfo.json"
 
-curl -v $URL -H "Cookie: ACT_SSO_COOKIE=$COOKIE_VALUE" -H "User-Agent: $USER_AGENT" -o $OUTPUT_FILE
+curl -v "$URL" -H "Cookie: ACT_SSO_COOKIE=$COOKIE_VALUE" -H "User-Agent: $USER_AGENT" -H "Accept: application/json, text/plain, */*" -H "Referer: https://profile.callofduty.com/" -H "Connection: keep-alive" -H "Accept-Language: en-US,en;q=0.9" --cookie-jar cookies.txt --cookie cookies.txt -o $OUTPUT_FILE
